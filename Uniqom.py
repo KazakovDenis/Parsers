2330b29e563f3c68ae1b4f9a61ebed14abe1a464
Praetorian <madglory@gmail.com>
2019-07-25 22:06:16 +0700

    - переименовал некоторые функции - добавил проход по пагинации - исправил извлечение товаров, теперь забирает все
    * не решена проблема с открытием/закрытием файла в декораторе
    * добавить парсинг через прокси
    * решить вопрос с декодированием

--- Uniqom.py
+++ Uniqom.py
@@ -1,107 +1,110 @@
-import requests    
-import csv
-import lxml
+import requests, csv, lxml
 from bs4 import BeautifulSoup  
 from datetime import datetime
 from multiprocessing import Pool
 
 
 def get_html(url):
-  '''Функция получения html-кода страницы по урл'''
-  html = requests.get(url)   # возвращает объект
-  return html.text           # возвращает строку
+  '''Р¤СѓРЅРєС†РёСЏ РїРѕР»СѓС‡РµРЅРёСЏ html-РєРѕРґР° СЃС‚СЂР°РЅРёС†С‹ РїРѕ СѓСЂР»'''
+  html = requests.get(url)   # РІРѕР·РІСЂР°С‰Р°РµС‚ РѕР±СЉРµРєС‚
+  return html.text           # РІРѕР·РІСЂР°С‰Р°РµС‚ СЃС‚СЂРѕРєСѓ
 
 
-def get_all_links(html): 
-  ''' Функция находит на странице входа ссылки на страницы для парсинга и выдаёт в виде списка '''
+def get_category_links(html):
+  ''' Р¤СѓРЅРєС†РёСЏ РЅР°С…РѕРґРёС‚ РЅР° СЃС‚СЂР°РЅРёС†Рµ РІС…РѕРґР° СЃСЃС‹Р»РєРё РЅР° СЃС‚СЂР°РЅРёС†С‹ РґР»СЏ РїР°СЂСЃРёРЅРіР° Рё РІС‹РґР°С‘С‚ РІ РІРёРґРµ СЃРїРёСЃРєР° '''
   soup = BeautifulSoup(html, 'lxml')
-  # получаем все теги <li> на странице с указанным классом
+  # РїРѕР»СѓС‡Р°РµРј РІСЃРµ С‚РµРіРё <li> РЅР° СЃС‚СЂР°РЅРёС†Рµ СЃ СѓРєР°Р·Р°РЅРЅС‹Рј РєР»Р°СЃСЃРѕРј
   lis = soup.find_all('li', class_='feip-productsList-level4-label')
   links = []
   for li in lis:
-      # получаем атрибут href тега <a>, находящегося внутри каждого <li>
+      # РїРѕР»СѓС‡Р°РµРј Р°С‚СЂРёР±СѓС‚ href С‚РµРіР° <a>, РЅР°С…РѕРґСЏС‰РµРіРѕСЃСЏ РІРЅСѓС‚СЂРё РєР°Р¶РґРѕРіРѕ <li>
       a = li.find('a').get('href')
       link = 'https://uniqom.ru' + a
       links.append(link)
   return links
 
 
+def write_csv(data, articul):
+  ''' Р¤СѓРЅРєС†РёСЏ Р·Р°РїРёСЃС‹РІР°РµС‚ РґР°РЅРЅС‹Рµ РІ csv'''
+  with open('uniqom.csv', 'a', newline='') as csv_file:
+    writer = csv.writer(csv_file, delimiter=';')
+    writer.writerow(data)
+  print(f'{articul} has been parsed')
+
+
 def get_category_data(html):
-  ''' Функция парсит страницы категории товаров и возвращает словарь с извлечёнными данными '''
+  ''' Р¤СѓРЅРєС†РёСЏ РїР°СЂСЃРёС‚ СЃС‚СЂР°РЅРёС†С‹ РєР°С‚РµРіРѕСЂРёРё С‚РѕРІР°СЂРѕРІ Рё РІРѕР·РІСЂР°С‰Р°РµС‚ СЃР»РѕРІР°СЂСЊ СЃ РёР·РІР»РµС‡С‘РЅРЅС‹РјРё РґР°РЅРЅС‹РјРё '''
   soup = BeautifulSoup(html, 'lxml')
-# извлекаем список товаров на странице, проходим по списку
-  goods_rows = soup.find('div', class_='feip-productsList-product-row')
-  data = {}
-  for goods_row in goods_rows:
-# извлекаем артикул
+# РёР·РІР»РµРєР°РµРј СЃРїРёСЃРѕРє С‚РѕРІР°СЂРѕРІ РЅР° СЃС‚СЂР°РЅРёС†Рµ, РїСЂРѕС…РѕРґРёРј РїРѕ СЃРїРёСЃРєСѓ
+  goods_rows = soup.find_all('div', class_='feip-productsList-product-row')
+  data = []
+  for each in goods_rows:
+# РёР·РІР»РµРєР°РµРј Р°СЂС‚РёРєСѓР»
     try:
-        articul = soup.find('div', class_='feip-productsList-product-articleID').text.strip()
+        articul = each.find('div', class_='feip-productsList-product-articleID').text.strip()
     except:
         articul = 'articul exception'
-# извлекаем изображение
+# РёР·РІР»РµРєР°РµРј РёР·РѕР±СЂР°Р¶РµРЅРёРµ
     try:
-        image = 'https:' + soup.find('div', class_='feip-productsList-photoCell').find('a').get('href')
+        image = 'https:' + each.find('div', class_='feip-productsList-photoCell').find('a').get('href')
         if 'bw-logotype.png' in image:
             image = 'no img'
     except:
         image = 'img exception'
-# извлекаем описание
+# РёР·РІР»РµРєР°РµРј РѕРїРёСЃР°РЅРёРµ
     try:
-        description = soup.find('div', class_='feip-productsList-product-variationLine').find('div').text.strip()
+        description = each.find('div', class_='feip-productsList-product-variationLine').find('div').text.strip()
     except:
         description = 'description exception'
-    data = {'articul': articul, 'image': image, 'description': description}
-
-    # переходим по пагинации
-    # follow_next_page(soup)
-  return data
+        
+    data = [articul, image, description]
+    write_csv(data, articul)
+    # РїРµСЂРµС…РѕРґРёРј РїРѕ РїР°РіРёРЅР°С†РёРё
+  follow_next_page(soup)
 
 
 def follow_next_page(soup):
-  pagination = soup.find('li', class_='uk-pagination-next').find('a').get('href')
-  next_page = 'https://uniqom.ru' + pagination
-  make_all(next_page)
+  '''Р¤СѓРЅРєС†РёСЏ РїРµСЂРµС…РѕРґРёС‚ РЅР° СЃР»РµРґСѓСЋС‰СѓСЋ СЃС‚СЂР°РЅРёС†Сѓ РІРЅСѓС‚СЂРё РєР°С‚РµРіРѕСЂРёРё С‚РѕРІР°СЂРѕРІ'''
+  try:
+    pagination = soup.find('li', class_='uk-pagination-next').find('a').get('href')
+    next_page = 'https://uniqom.ru' + pagination
+    parse_pages(next_page)
+  except:
+    pass
 
 
-def write_csv(data):
-  ''' Функция записывает данные в csv'''
-  with open('uniqom.csv', 'a') as f:
-    writer = csv.writer(f)
-    writer.writerow( (data['articul'], data['image'], data['description']) ) 
-  # принимает 1 аргумент, поэтому кортеж
-    print(data['articul'], 'is parsed')
-
-
-# пишем функцию для параллельного парсинга
-def make_all(url):
+# РїРёС€РµРј С„СѓРЅРєС†РёСЋ РґР»СЏ РїР°СЂР°Р»Р»РµР»СЊРЅРѕРіРѕ РїР°СЂСЃРёРЅРіР°
+def parse_pages(url):
   html = get_html(url)
-  data = get_category_data(html)
-  write_csv(data)
+  get_category_data(html)
 
 
-# позже вынесу подсчёт времени выполнения в декоратор @time_counter 
+def time_file_decorator(func):
+  ''' Р”РµРєРѕСЂР°С‚РѕСЂ СЃС‡РёС‚Р°РµС‚ РІСЂРµРјСЏ РІС‹РїРѕР»РЅРµРЅРёСЏ РІСЃРµРіРѕ СЃРєСЂРёРїС‚Р° Рё РѕС‚РєСЂС‹РІР°РµС‚/Р·Р°РєСЂС‹РІР°РµС‚ С„Р°Р№Р» РґР»СЏ Р·Р°РїРёСЃРё'''
+  def wrapper():
+    # РЅР°С‡РёРЅР°РµРј РѕС‚СЃС‡С‘С‚ РІСЂРµРјРµРЅРё СЂР°Р±РѕС‚С‹ РјРѕРґСѓР»СЏ
+    start = datetime.now()
+    print(f'Parsing started at {start}')
+    # РЅРµ СѓРґР°Р»РѕСЃСЊ СЂРµС€РёС‚СЊ РїСЂРѕР±Р»РµРјСѓ РІС‹РЅРѕСЃР° РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р° РІ РґРµРєРѕСЂР°С‚РѕСЂ
+    # С„Р°Р№Р» С‡РёС‚Р°РµС‚СЃСЏ, РЅРѕ РЅРµ РїРёС€РµС‚СЃСЏ
+    # global csv_file
+    # with open('uniqom.csv', 'a', newline='') as csv_file:
+    func()
+    # СЃС‡РёС‚Р°РµРј Рё РІС‹РІРѕРґРёРј Р·Р°С‚СЂР°С‡РµРЅРЅРѕРµ РІСЂРµРјСЏ
+    period = datetime.now() - start
+    print('Time wasted: ', str(period))
+  return wrapper
+
+
+@time_file_decorator
 def main():
-  # начинаем отсчёт времени работы модуля
-  start = datetime.now()
-
-  # получаем на странице входа список ссылок на страницы категорий 
+  # РїРѕР»СѓС‡Р°РµРј РЅР° СЃС‚СЂР°РЅРёС†Рµ РІС…РѕРґР° СЃРїРёСЃРѕРє СЃСЃС‹Р»РѕРє РЅР° СЃС‚СЂР°РЅРёС†С‹ РєР°С‚РµРіРѕСЂРёР№ 
   url = 'https://uniqom.ru/catalog'
-  all_links = get_all_links( get_html(url) )
+  all_links = get_category_links( get_html(url) )
 
-  # код для парсинга 4-мя параллельными процессами
-  with Pool(1) as p:
-      p.map(make_all, all_links)
-
-  # код для последовательного парсинга одним процессом
-  #categories_count = len(all_links)      
-  #for index, url in enumerate(all_links):
-    #html = get_html(url)
-    #data = get_category_data(html)
-    #write_csv(data)
-    #print(f'Прогресс: {index} / {categories_count}')  
-
-  period = datetime.now() - start
-  print('Время парсинга: ', str(period))
+  # РєРѕРґ РґР»СЏ РїР°СЂСЃРёРЅРіР° N РїР°СЂР°Р»Р»РµР»СЊРЅС‹РјРё РїСЂРѕС†РµСЃСЃР°РјРё
+  with Pool(4) as p:
+      p.map(parse_pages, all_links[1:2]) 
 
 
 if __name__ == '__main__':
