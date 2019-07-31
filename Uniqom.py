4483c66b1d8fd5375b1e72738a1cd17c8b629cb9
Praetorian <madglory@gmail.com>
2019-07-31 18:24:46 +0700

    Добавил: - задержку при запросе и её увеличение при повторном запросе - таймауты запросов - запросы через прокси и замену юзер-агентов - прокси подменяется при запросе свыше таймаута или бане
    Сделать:
    * заменить переход по пагинации подстановкой page= в ссылку во избежание вылета из категории из-за таймаута или ответа None
    * прокси выдавать не через choice, а генератором с использованием yield, чтобы исключить повторения

--- Uniqom.py
+++ Uniqom.py
@@ -1,94 +1,152 @@
-import requests, csv, lxml
+﻿''' Скрипт извлекает из сайта поставщика артикулы, ссылки на изображения и описания товаров:
+- находим в каталоге ссылки на страницы категорий товаров
+- заходим в категорию, парсим товары
+- находим пагинацию, переходим на следующую страницу
+- если последняя страница / нет товаров переходим в следующую категорию '''
+# lxml не используется, но без неё скрипт не работает
+
+# добавил задержку при парсинге
+# добавил прокси и юзер-агентов
+# реализовал замену прокси только при бане
+
+# ///заменить переход по пагинации
+# //// сделать генератор прокси с yield
+
+
+import requests, csv, certifi
+import lxml
 from bs4 import BeautifulSoup  
 from datetime import datetime
 from multiprocessing import Pool
+from random import choice, uniform
+from time import sleep
 
 
-def get_html(url):
-  '''Функция получения html-кода страницы по урл'''
-  html = requests.get(url)   # возвращает объект
-  return html.text           # возвращает строку
+# страница входа 
+url = 'https://uniqom.ru/catalog'
+# маскируемся: исходные данные для requests.get
+default_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+default_proxy = {'http': 'http://41.89.171.220:8080', 'https': 'http://35.244.20.250:80'}
 
 
-def get_category_links(html):
-  ''' Функция находит на странице входа ссылки на страницы для парсинга и выдаёт в виде списка '''
-  soup = BeautifulSoup(html, 'lxml')
+def get_response(url, useragent = default_agent, proxy = default_proxy):
+  '''Полученаем ответ страницы по урл с паузой '''
+  print('Trying to parse', url)
+  timeout = uniform(35, 40)
+  try:
+    response = requests.get(url, headers=useragent, proxies=proxy, timeout=timeout, verify=certifi.where())
+    # делаем паузу
+    pause = uniform(3, 6)
+    sleep(pause)
+    return response
+  # в случае бана меняем на случайные прокси и юзер-агент
+  except requests.exceptions.RequestException as e:
+    print(f'Error for {url}. More info:', e)
+    useragent = {'User-Agent': choice(useragents_list)}
+    proxy = {'http': choice(proxy_list)}
+    # пробуем снова с бОльшей паузой
+    pause = uniform(10, 15)
+    sleep(pause)
+    get_response(url, useragent, proxy)
+  except:
+    # непредусмотренные ошибки
+    print(f'Another Error for {url} in get_response().')
+    return None
+
+
+def get_category_links(dom):
+  ''' Находим на странице входа ссылки на страницы для парсинга и выдаёт в виде списка '''
+  soup = BeautifulSoup(dom, 'lxml')
   # получаем все теги <li> на странице с указанным классом
   lis = soup.find_all('li', class_='feip-productsList-level4-label')
   links = []
   for li in lis:
-      # получаем атрибут href тега <a>, находящегося внутри каждого <li>
-      a = li.find('a').get('href')
-      link = 'https://uniqom.ru' + a
-      links.append(link)
+    # получаем атрибут href тега <a>, находящегося внутри каждого <li>
+    a = li.find('a').get('href')
+    link = 'https://uniqom.ru' + a
+    links.append(link)
   return links
 
 
-def write_csv(data, articul):
-  ''' Функция записывает данные в csv'''
+def write_csv(data):
+  ''' Записываем данные в csv'''
   with open('uniqom.csv', 'a', newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter=';')
     writer.writerow(data)
-  print(f'{articul} has been parsed')
 
 
-def get_category_data(html):
-  ''' Функция парсит страницы категории товаров и возвращает словарь с извлечёнными данными '''
-  soup = BeautifulSoup(html, 'lxml')
-# извлекаем список товаров на странице, проходим по списку
-  goods_rows = soup.find_all('div', class_='feip-productsList-product-row')
-  data = []
-  for each in goods_rows:
-# извлекаем артикул
-    try:
+def get_category_data(response):
+  ''' Парсим страницы категории товаров и пишем в csv '''
+  try:
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = []
+  # извлекаем список товаров на странице, проходим по списку
+    goods_rows = soup.find_all('div', class_='feip-productsList-product-row')
+    for each in goods_rows:
+      # извлекаем артикул
+      try:
         articul = each.find('div', class_='feip-productsList-product-articleID').text.strip()
-    except:
+      except:
         articul = 'articul exception'
-# извлекаем изображение
-    try:
+      # извлекаем изображение
+      try:
         image = 'https:' + each.find('div', class_='feip-productsList-photoCell').find('a').get('href')
         if 'bw-logotype.png' in image:
-            image = 'no img'
-    except:
+          image = 'no img'
+      except:
         image = 'img exception'
-# извлекаем описание
-    try:
+      # извлекаем описание
+      try:
         description = each.find('div', class_='feip-productsList-product-variationLine').find('div').text.strip()
-    except:
+      except:
         description = 'description exception'
-        
-    data = [articul, image, description]
-    write_csv(data, articul)
-    # переходим по пагинации
-  follow_next_page(soup)
+      # сохраняем в data, data пишем в csv
+      data = [articul, image, description]
+      write_csv(data)
+      # ---------- конец тела цикла извлечения параметров товаров
+    print(f'Page "{response.url}" has been parsed')
+    # ищем пагинацию и переходим по ней
+    follow_next_page(soup)
+  except:
+    print(f'No data here {response.url}')
 
 
 def follow_next_page(soup):
-  '''Функция переходит на следующую страницу внутри категории товаров'''
+  '''Переходим на следующую страницу внутри категории товаров'''
   try:
     pagination = soup.find('li', class_='uk-pagination-next').find('a').get('href')
     next_page = 'https://uniqom.ru' + pagination
     parse_pages(next_page)
   except:
-    pass
+    title = soup.find('title').text.strip()
+    print(f'"{title}" has been parsed')
 
 
 # пишем функцию для параллельного парсинга
 def parse_pages(url):
-  html = get_html(url)
-  get_category_data(html)
+  # оставляем только локальные склады
+  if not 'deposits=2' in url:
+    url += '?&deposits=2&limit=100'
+  # входим на страницу и парсим её
+  try:
+    response = get_response(url)
+    assert response != None, f'{url} is returned as None'
+    if response.status_code == 200:
+      get_category_data(response)
+    else:
+      print(f'Status {response.status_code} of page {url}')
+  except AssertionError as a:
+    print(a)
+  except:
+    print(f'Another trouble with {url}')
 
 
 def time_file_decorator(func):
-  ''' Декоратор считает время выполнения всего скрипта и открывает/закрывает файл для записи'''
+  ''' Декоратор считает время выполнения всего скрипта '''
   def wrapper():
     # начинаем отсчёт времени работы модуля
     start = datetime.now()
     print(f'Parsing started at {start}')
-    # не удалось решить проблему выноса открытия файла в декоратор
-    # файл читается, но не пишется
-    # global csv_file
-    # with open('uniqom.csv', 'a', newline='') as csv_file:
     func()
     # считаем и выводим затраченное время
     period = datetime.now() - start
@@ -98,16 +156,25 @@ def time_file_decorator(func):
 
 @time_file_decorator
 def main():
+  # объявляем глобально данные для избежания бана 
+  global useragents_list, proxy_list
+
+  # получаем список юзер-агентов из файла
+  with open('useragents.txt') as useragents_file:
+    useragents_list = useragents_file.read().split('\n')
+
+  # получаем список прокси из файла (прокси проверять отдельно!)
+  with open('proxylist.txt') as proxy_file:
+    proxy_list = proxy_file.read().split('\n')
+
   # получаем на странице входа список ссылок на страницы категорий 
-  url = 'https://uniqom.ru/catalog'
-  all_links = get_category_links( get_html(url) )
+  index = get_response(url).text
+  category_links = get_category_links(index) # 271 категория
 
   # код для парсинга N параллельными процессами
-  with Pool(4) as p:
-      p.map(parse_pages, all_links[1:2]) 
+  with Pool(1) as p:
+      p.map(parse_pages, category_links) 
 
 
 if __name__ == '__main__':
     main()
-    
-
